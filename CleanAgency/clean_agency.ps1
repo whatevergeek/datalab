3316e2e75835138aca06a72e520d2106d152b686
whatevergeek <whatevergeek@gmail.com>
2015-04-04 09:12:31 +0800

    Added stop words
    Added handling for stop words

--- CleanAgency/clean_agency.ps1
+++ CleanAgency/clean_agency.ps1
@@ -1,23 +1,50 @@
-$agency_list = {(gc agency_nm) -split '#'}.Invoke()
+## Configuration: Start
+
+$max_distance_percent = 15
+$distance_counter = "C:\Users\PaulLorett\Desktop\FromW510\20150403\CleanAgency\get-ld.ps1"
+$agency_file = "C:\Users\PaulLorett\Desktop\FromW510\20150403\CleanAgency\agency_nm"
+$stopwords_location = "C:\Users\PaulLorett\Desktop\FromW510\20150403\CleanAgency\stopwords.txt"
+
+## Configuration: End
+
+$agency_list = {(gc $agency_file) -split '#'}.Invoke()
+$stopwords = (gc $stopwords_location)
 $agency_map = @{}
+
+function Remove-StopWords([string]$name, $stopwords)
+{
+    $new_name = $name.ToLower()
+
+    foreach($word in $stopwords)
+    {
+        $new_name = $new_name.Replace($word.ToLower(), "")
+    }
+
+    return $new_name
+}
 
 while($agency_list.Count -gt 0)
 {
-    $agency_list.Count
+    #$agency_list.Count
     $agency = $agency_list[0]
     [void]$agency_list.Remove($agency)
 
     [void]$agency_map.Add($agency, $agency)
-    $agency
+    #$agency
 
     $agency_list2 = $agency_list | foreach{$_}
     $agency_list2 = {$agency_list2}.Invoke()
 
+    $base_agency = Remove-StopWords $agency $stopwords
+    $max_distance_step = [Math]::Round($base_agency.Length*($max_distance_percent/100))
     while($agency_list2.Count -gt 0)
     {
-        if((.\get-ld.ps1 $agency_list2[0] $agency) -lt 5)
+        $other_agency = Remove-StopWords $agency_list2[0] $stopwords
+
+
+        if((&$distance_counter $other_agency $base_agency) -le $max_distance_step)
         {
-            $agency_list2[0]
+            #$agency_list2[0]
             [void]$agency_map.Add($agency_list2[0], $agency)
             [void]$agency_list.Remove($agency_list2[0])
         }
@@ -25,3 +52,6 @@ while($agency_list.Count -gt 0)
         [void]$agency_list2.Remove($agency_list2[0])
     }
 }
+
+$agency_map.GetEnumerator() | sort Name | select @{Label="Agency_Name";Expression={$_.Name}}, @{Label="Common_Name";Expression={$_.Value}}|Export-Csv "agency_to_commonname_mapping.csv" -NoTypeInformation
+$agency_map.GetEnumerator() | sort Value | select Value -Unique | %{$_.Value} | Out-File "unique_agency_names.txt"
